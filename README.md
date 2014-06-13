9fb615eebe6922dcf9cb458941f2846a13c277bb
Shivraj Rath <shivrajrath@gmail.com>
2014-06-12 21:08:55 -0700

    Writable stream to file

---
+++ README.md
@@ -0,0 +1,13 @@
+## Node Feed Parsing
+
+
+This is a sample code to demo RSS feed parsing using nodeJS.
+
+NPM Dependencies:
+
+* feedparser
+* request
+
+**Objective**
+* Parse feed into a text file
+* HTML response parsing
---
+++ app.js
@@ -0,0 +1,54 @@
+// NPM Packages
+var request = require('request'),
+	FeedParser = require('feedparser'),
+	fs = require('fs'),
+
+var req = request('http://nodeweekly.com/rss/21ok1ccc'),
+	feedparser = new FeedParser();
+
+
+
+// Error on request
+req.on('error', function( error ) {
+	console.log( 'Request Error ' + error );
+});
+
+// Any response for the request
+req.on('response', function( res ) {
+	// This is a readable stream
+	var stream = this;
+	
+	// Bad status code
+	if ( res.statusCode !== 200){
+		return this.emit('error', new Error('Bad status code'));
+	}
+	
+	// Writing to feedparser writable stream	
+	stream.pipe(feedparser);
+});
+
+
+// Feed parser error
+feedparser.on('error', function(err){
+	console.log('Feed error ' + err);
+})
+
+// Readable event of feedparser writable stream
+feedparser.on('readable', function(){
+
+	var stream = this,
+		meta = this.meta,
+		item;
+
+	// Write to the the text file till writeable ends
+	while(item = stream.read()){
+		fs.appendFile('write.txt', JSON.stringify(item), function (err) {
+			if (err){
+				console.log('Error in writing file' + err);
+			}else{
+				console.log('Writing to file ...')
+			}
+		});
+	}
+})
+
